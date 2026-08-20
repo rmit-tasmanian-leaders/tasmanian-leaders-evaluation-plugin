77bdbb1f1c2a4c5684ce4b3c1f34d1ded704764c
Dani Adonai <adonaidani30@gmail.com>
2026-08-20 13:40:28 +1000

    chore: create initial WordPress plugin structure

---
+++ tasmanian-leaders-evaluation.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Tasmanian Leaders Evaluation Plugin
+ * Description: Evaluation reporting plugin for Tasmanian Leaders.
+ * Version: 0.1.0
+ * Author: Team 55
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
